1072a5dfdb2aebca2b9b8a42e4349872c1fa2134
Alejandro Melara <alejandro.melara96@gmail.com>
2022-11-15 09:33:09 -0500

    Variable Weather Patterns
    JS-1

---
+++ index.js
@@ -0,0 +1,55 @@
+
+const companyName = `Sonny Daeye`;
+console.log(`What's the company name`, companyName)
+
+let isSunny; 
+isSunny = false ; 
+console.log('Is it sunny?' , isSunny);
+
+isSunny = true;
+console.log('Is it sunny?' , isSunny);
+
+'Space'
+
+let isRaining;
+isRaining = true ;
+console.log('Is it raining?', isRaining)
+
+isRaining = false;
+console.log('Is it raining?', isRaining)
+
+'Space'
+
+let windSpeed;
+windSpeed = `14km/h` ;
+console.log("What's the wind speed?",windSpeed)
+
+windSpeed = '10km/h'
+console.log("What's the wind speed?",windSpeed)
+
+'Space'
+
+let windDirection;
+windDirection = "SE" ;
+console.log(`What's the wind direction?`, windDirection)
+
+windSpeed = 'NW';
+console.log(`What's the wind direction?`, windDirection)
+
+'Space'
+
+let dayOfWeek;
+dayOfWeek = "Wednesday" ;
+console.log('What day is today?', dayOfWeek)
+
+dayOfWeek = 'Thursday'
+console.log('What day is today?', dayOfWeek)
+
+'Space'
+
+let expectedPrecipitation;
+expectedPrecipitation = '6mm';
+console.log(`What's the expected precipitation?`, expectedPrecipitation)
+
+expectedPrecipitation = '4mm';
+console.log(`What's the expected precipitation?`, expectedPrecipitation)
